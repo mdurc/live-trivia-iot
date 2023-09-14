53553faee70af9f41af1c605392f754693f3bdae
mdurc <thisistheactualdark@gmail.com>
2023-09-13 22:32:10 -0400

    timer based trivia, ran by server, everyone on the website sees the same questions at the same time. Usernames and highscores and passwords are stored on local machines and on the server. Multiple choice questions.

--- public/script.js
+++ public/script.js
@@ -2,23 +2,219 @@ const questionContainer = document.getElementById('question-container');
 const questionText = document.getElementById('question-text');
 const answerButtons = document.getElementById('answer-buttons');
 const options = document.getElementsByClassName('Answerbtn');
+const main = document.getElementsByClassName('main');
 const streakValue = document.getElementById('streak');
 const categoryTitle = document.getElementById('category');
-const highScoreValue = document.getElementById('highscore'); // Add this line
+
+const historyList = document.getElementById('history-list');
+
+const leaderboardList = document.getElementById('leaderboard-list');
+
 const countdownElement = document.getElementById('countdown-timer');
 
 let countdown = 10;
 let streak = 0;
-let highScore = 0;
 let optionClicked = false
 let correctAnswerCheck = "null";
 
-let randomIndex = Math.floor(Math.random() * 4);
-// Function to update the question
+let username = "";
+let password = "";
 
+let randomIndex = Math.floor(Math.random() * 4);
+
+let userHighscore = 0;
+
+const loginContainer = document.getElementById('login-container');
+const usernameInput = document.getElementById('username');
+const passwordInput = document.getElementById('password');
+const startButton = document.getElementById('start-button');
+
+
+
+
+
+const setNewHighScore = (username, highscore) => {
+    console.log(username + " HIGHSCORE: "+ highscore)
+    console.log(username, highscore)
+    console.log(usernameInput.value.trim())
+    sendScoreToServer(username, highscore);
+    
+    updateLeaderboard();
+};
+
+const checkUsername = (username, password) => {
+    fetch('/check-username', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+    })
+    .then((response) => response.json())
+    .then((data) => {
+        if (data.exists) {
+            // Username exists, check the password
+            if (data.correctPassword) {
+                // Correct password, proceed to the game
+                showTrivia();
+            } else {
+                alert('Incorrect password. Please try again.');
+            }
+        } else {
+            // Username doesn't exist, register the user
+            const newUser = { username, password, highscore: 0 };
+            const usersData = JSON.parse(localStorage.getItem('usersData')) || [];
+            let actualUser;
+            const userExists = usersData.some((user) => {
+                if (user.username === username) {
+                    console.log(user); // Print the specific user when found
+                    console.log(user.password);
+                    console.log(user.password !== password);
+                    actualUser = user;
+                    return true; // Return true to indicate that the user exists
+                }
+                return false;
+            });
+            
+            if (userExists) {
+                if(actualUser.password !== password){
+                    alert('Incorrect Password');
+                    console.log("inputted: " + password);
+                }else{
+                    showTrivia();
+                }
+            }else{
+                // Add the new user to the usersData array
+                registerUser(username, password);
+                sendScoreToServer(username, 0);
+                usersData.push(newUser);
+                // Save the updated usersData array in local storage
+                localStorage.setItem('usersData', JSON.stringify(usersData));
+            }
+        }
+    })
+    .catch((error) => {
+        console.error('Error checking username:', error);
+    });
+};
+
+const registerUser = (username, password) => {
+    fetch('/register', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+    })
+    .then((response) => response.json())
+    .then((data) => {
+        if (data.success) {
+            // User registered successfully, proceed to the game
+            showTrivia();
+        } else {
+            alert('Failed to register user. Please try again.');
+        }
+    })
+    .catch((error) => {
+        console.error('Error registering user:', error);
+    });
+};
+
+const clearUsers = () => {
+    fetch('/clear-users', {
+        method: 'DELETE',
+    })
+        .then((response) => {
+            localStorage.clear()
+            if (response.status === 200) {
+                console.log('Users and leaderboard data cleared successfully');
+            } else {
+                console.error('Failed to clear users and leaderboard data');
+            }
+        })
+        .catch((error) => {
+            console.error('Error clearing users and leaderboard data:', error);
+        });
+};
+
+
+const showTrivia = () => {
+    historyList.innerHTML = '';
+    main[0].style.display = 'flex';
+    loginContainer.style.display = 'none'; // Hide the username container
+    questionContainer.style.display = 'block'; // Show the trivia questions container
+    updateQuestion(); // Start fetching and displaying questions
+};
+
+const sendScoreToServer = (username, score) => {
+    fetch('/score', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, score }),
+    })
+    .then((response) => {
+        if (response.status === 200) {
+            console.log('Score recorded successfully');
+        } else {
+            console.error('Failed to record score');
+        }
+    })
+    .catch((error) => {
+        console.error('Error sending score:', error);
+    });
+};
+
+for (let j = 0; j < options.length; j++) {
+  options[j].disabled = true;
+}
+
+// Add a click event listener to the "Start" button
+startButton.addEventListener('click', () => {
+    username = usernameInput.value.trim();
+    password = passwordInput.value.trim();
+    
+    if (username.length === 0 || password.length === 0) {
+        alert('Please enter both a valid username and password.');
+    } else {
+        // Check if the username exists on the server
+        console.log(username, password);
+        checkUsername(username, password);
+    }
+});
+
+
+
+const updateLeaderboard = () => {
+    console.log("updating leaderboard");
+    const tempLeader = document.createElement('ul');
+    tempLeader.id = 'leaderboard';
+    tempLeader.classList.add('list');
+
+    // Sort the leaderboard by high score
+    fetch('/leaderboard')
+        .then((response) => response.json())
+        .then((leaderboardData) => {
+            // Sort the leaderboard by score in descending order
+            leaderboardData.sort((a, b) => b.score - a.score);
+
+            // Update the leaderboard UI with the sorted entries
+            leaderboardData.forEach((entry, index) => {
+                const listItem = document.createElement('li');
+                listItem.innerHTML = `${index + 1}. ${entry.username}: <span>${entry.score}</span>`;
+                tempLeader.appendChild(listItem);
+            });
+            if(leaderboardList.innerHTML !== tempLeader.innerHTML){
+                leaderboardList.innerHTML = tempLeader.innerHTML;
+            }
+        })
+        .catch((error) => {
+            console.error('Error fetching leaderboard data:', error);
+        });
+};
 
 const addItemToHistory = (itemText) => {
-    const historyList = document.getElementById('history-list');
     const listItem = document.createElement('li');
     listItem.textContent = itemText;
     historyList.appendChild(listItem);
@@ -33,16 +229,19 @@ const handleButtonClick = (button, correctAnswer) => {
     button.style.backgroundColor = 'green';
     streak++; // Increase streak
       streakValue.textContent = streak; // Update streak display
-      addItemToHistory("🟩Correct!🟩");
+      addItemToHistory("✔ Correct! ✔ Answer: "+ correctAnswerCheck);
+      console.log("streak: "+ streak);
+      console.log("highscoreforuser: "+ userHighscore);
 
     // Update high score if current streak is higher
   } else {
     // Incorrect answer clicked
     button.style.backgroundColor = 'red';
-      addItemToHistory("🟥Incorrect🟥")
-      if (streak > highScore) {
-        highScore = streak;
-        highScoreValue.textContent = highScore; // Update high score display
+      addItemToHistory("✘ Incorrect ✘ Answer: "+ correctAnswerCheck)
+      getHighscore(username)
+      console.log("userHighScore: " + userHighscore)
+      if (streak > userHighscore) {
+          setNewHighScore(username, streak)
       }
     streak = 0; // Reset streak
     streakValue.textContent = streak; // Update streak display
@@ -72,41 +271,53 @@ const updateQuestion = () => {
         const currentTime = Date.now();
         const serverTimestamp = data.timestamp;
         
-        console.log("curr: "+ currentTime)
-        console.log("server: "+ serverTimestamp)
-        console.log("diff: "+ (currentTime-serverTimestamp))
         countdownElement.textContent = Math.max(0, Math.floor((10000 - (currentTime - serverTimestamp)) / 1000) + 1);
         
 
         if (currentTime - serverTimestamp >= 10000 && currentTime - serverTimestamp <10500) {
             if(!optionClicked){
-                if (streak > highScore) {
-                  highScore = streak;
-                  highScoreValue.textContent = highScore; // Update high score display
+                getHighscore(username)
+                console.log("userHighScore: " + userHighscore)
+                if (streak > userHighscore) {
+                    setNewHighScore(username, streak) // Update high score display
                 }
               streak = 0; // Reset streak
               streakValue.textContent = streak; // Update streak display
-                addItemToHistory("🟥Did not answer🟥")
+                if(correctAnswerCheck!="null"){
+                    addItemToHistory("⏱ Timeout ⏱ Answer: "+ correctAnswerCheck)
+                }
             }
             
             for (let j = 0; j < options.length; j++) {
-              options[j].disabled = false;
+                options[j].disabled = false;
                 options[j].style.backgroundColor = '';
+                options[j].textContent = data.data[j].answer;
+                console.log("Option "+ j+ ":" + data.data[j].answer);
             }
             
-            console.log(data.timestamp)
-            console.log(data.data[0].category);
-            console.log("");
-            console.log(data.data[0].question);
-            console.log("");
-            console.log(data.data[0].answer);
             
-            correctAnswerCheck = data.data[0].answer;
-            randomIndex = Math.floor(Math.random() * 4);
+            //set current answer
+            fetch('/question-index', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            })
+            .then((response) => response.json())
+            .then((result) => {
+                console.log("ANSWER: "+ data.data[result.number].answer);
+                
+                correctAnswerCheck = data.data[result.number].answer;
+                
+                categoryTitle.textContent = "Category: " + data.data[result.number].category;
+                
+                questionText.textContent = data.data[result.number].question;
+            })
+            .catch((error) => {
+                console.error('Error fetching random number:', error);
+            });
             
-            categoryTitle.textContent = "Category: " + data.data[0].category;
-            options[randomIndex].textContent = data.data[0].answer;
-            questionText.textContent = data.data[0].question;
+            
             optionClicked = false;
         }
     })
@@ -115,8 +326,84 @@ const updateQuestion = () => {
     });
 };
 
+const getHighscore = (username) => {
+    if(username.length > 0){
+        fetch(`/highscore?username=${username}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        })
+        .then((response) => {
+            if (response.status === 200) {
+                // Parse the JSON response to get the highscore data
+                return response.json();
+            } else if (response.status === 404) {
+                // User not found in leaderboard, set highscore to 0
+                return { username, highscore: 0 };
+            } else {
+                console.log('Failed to retrieve highscore, defaulting to 0');
+                return { username, highscore: 0 };
+            }
+        })
+        .then((data) => {
+            if (data) {
+                // Parse the highscore as an integer
+                const usersData = JSON.parse(localStorage.getItem('usersData')) || [];
+
+                // Find the user with the matching username
+                const userToUpdate = usersData.find((user) => user.username === username);
+
+                if (userToUpdate) {
+                    // Update the user's highscore
+                    userToUpdate.highscore = parseInt(data.highscore);
+
+                    // Save the updated usersData array back to local storage
+                    localStorage.setItem('usersData', JSON.stringify(usersData));
+                } else {
+                    console.error('User not found');
+                }
+                userHighscore = parseInt(data.highscore);
+                console.log(`Server highscore for ${data.username}: ${userHighscore}`);
+            }
+        })
+        .catch((error) => {
+            console.error('Error retrieving highscore:', error);
+        });
+    }
+};
+
+const sendUsersDataToServer = (usersData) => {
+    fetch('/send-users-data', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ usersData }),
+    })
+    .then((response) => {
+        if (response.status === 200) {
+            console.log('Users data sent successfully');
+        } else {
+            console.error('Failed to send users data');
+        }
+    })
+    .catch((error) => {
+        console.error('Error sending users data:', error);
+    });
+};
+
+
+
+
+
 // Initial question update
 updateQuestion();
+updateLeaderboard();
 
-// Update the question every 10 seconds
+const usersData = JSON.parse(localStorage.getItem('usersData')) || [];
+sendUsersDataToServer(usersData);
+
+
+setInterval(updateLeaderboard,5000)
 setInterval(updateQuestion, 500);
--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express');
-const cors = require('cors');
+const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 3000;
 
+
+app.use(bodyParser.json());
+
 let responseData;
 let serverQuestionTime;
+
+let leaderboard = [];
+let userDatabase = {};
+
+let randomAnswerIndex = 0;
 
 const axios = require('axios');
 
@@ -12,32 +20,28 @@ const axios = require('axios');
 const options = {
   method: 'GET',
   url: 'https://trivia-by-api-ninjas.p.rapidapi.com/v1/trivia',
-  params: {
-    category: '',
-    limit: '1'
-  },
+  params: {limit: '4'},
   headers: {
-    'X-RapidAPI-Key': '8f8ffaa056mshe1d2054858d2cdap19754fjsnf4dabe271718',
+    'X-RapidAPI-Key': '81c58a39b9msh62f6fa945e90cc8p19590bjsnfeab9f319bcb',
     'X-RapidAPI-Host': 'trivia-by-api-ninjas.p.rapidapi.com'
   }
 };
 
 
 const fetchTriviaData = async () => {
+    console.log(userDatabase);
+    randomAnswerIndex = Math.floor(Math.random()*4);
     try {
         const response = await axios.request(options);
         responseData = response.data
-        console.log(response.data[0].question);
+        console.log(response.data[randomAnswerIndex].question);
         serverQuestionTime = Date.now()
     } catch (error) {
         console.error(error);
     }
 };
 
-setTimeout(() => {
 
-   
-}, 3000);
 
 setInterval(fetchTriviaData, 11000);
 
@@ -49,6 +53,121 @@ app.use(express.static('public'));
 app.get('/api/trivia', (req, res) => {
     
     res.json({ timestamp: serverQuestionTime, data: responseData });
+});
+
+app.get('/question-index', (req, res) => {
+    res.status(200).json({ number: randomAnswerIndex });
+});
+
+app.get('/leaderboard', (req, res) => {
+    // Assuming you have a 'leaderboard' array on the server
+    res.status(200).json(leaderboard);
+});
+
+app.get('/highscore', (req, res) => {
+    const { username } = req.query; // Get the username from the query parameters
+
+    if (!username) {
+        res.status(400).json({ message: 'Username is required' });
+        return;
+    }
+
+    // Find the highest score for the specified username in the leaderboard
+    const userScores = leaderboard.filter(entry => entry.username === username);
+
+    if (userScores.length === 0) {
+        // User not found in the leaderboard
+        res.json({ username, highscore: -1 });
+    } else {
+        // Calculate the highest score for the user and parse it as an integer
+        const highscore = Math.max(...userScores.map(entry => entry.score));
+        res.status(200).json({ username, highscore: parseInt(highscore) });
+    }
+});
+
+
+
+
+
+app.post('/send-users-data', (req, res) => {
+    const { usersData } = req.body;
+
+    if (Array.isArray(usersData)) {
+        // Print the received usersData array to the server console
+        console.log('Received usersData:', usersData);
+        for (let i = 0; i < usersData.length; i++) {
+            const { username, highscore } = usersData[i];
+            if (!leaderboard.some((user) => user.username === username)) {
+                leaderboard.push({ username, score: highscore });
+            }
+        }
+
+        res.status(200).json({ message: 'Users data received successfully' });
+    } else {
+        res.status(400).json({ message: 'Invalid request' });
+    }
+});
+
+
+
+
+
+app.post('/score', (req, res) => {
+    const { username, score } = req.body;
+
+    if (!username || typeof score !== 'number') {
+        res.status(400).json({ message: 'Invalid request' });
+        return;
+    }
+
+    // Check if the username already exists in the leaderboard
+    const existingEntryIndex = leaderboard.findIndex(entry => entry.username === username);
+
+    if (existingEntryIndex !== -1) {
+        // Update the score for the existing username
+        leaderboard[existingEntryIndex].score = score;
+        res.status(200).json({ message: 'Score updated successfully' });
+    } else {
+        // Add a new entry for the username and score
+        leaderboard.push({ username, score });
+        res.status(200).json({ message: 'Score recorded successfully' });
+    }
+});
+
+
+
+
+app.post('/check-username', (req, res) => {
+    const { username, password } = req.body;
+    if (userDatabase[username]) {
+        // Username exists, check the password
+        const correctPassword = userDatabase[username] === password;
+        res.json({ exists: true, correctPassword });
+    } else {
+        // Username doesn't exist
+        res.json({ exists: false });
+    }
+});
+
+
+app.post('/register', (req, res) => {
+    const { username, password } = req.body;
+    
+    if (userDatabase[username]) {
+        // Username already exists
+        res.json({ success: false });
+    } else {
+        // Register the new user
+        userDatabase[username] = password;
+        res.json({ success: true });
+    }
+});
+
+app.delete('/clear-users', (req, res) => {
+    userDatabase = {};
+    leaderboard = [];
+
+    res.status(200).json({ message: 'User data and leaderboard cleared successfully' });
 });
 
 app.listen(port, () => {
